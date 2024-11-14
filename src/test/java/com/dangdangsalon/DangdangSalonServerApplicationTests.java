5ad76262c941c0c8ebf97759faa17c86d1aec224
이도현 <lucky77795@naver.com>
2024-11-14 23:29:46 +0900

    init project

---
+++ src/test/java/com/dangdangsalon/DangdangSalonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangdangsalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangdangSalonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

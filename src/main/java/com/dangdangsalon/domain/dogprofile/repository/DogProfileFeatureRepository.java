13de28cc31172eb618f54ecd0886c0d45c04b0d4
99MinSu <dlalstn1023@naver.com>
2024-11-20 01:06:07 +0900

    Refactor: 강아지 프로필에 연결된 특징 및 견적 요청한 서비스 조회 N+1 문제 JOIN FETCH 로 해결

--- src/main/java/com/dangdangsalon/domain/dogprofile/repository/DogProfileFeatureRepository.java
+++ src/main/java/com/dangdangsalon/domain/dogprofile/repository/DogProfileFeatureRepository.java
@@ -4,10 +4,16 @@ package com.dangdangsalon.domain.dogprofile.repository;
 import com.dangdangsalon.domain.dogprofile.entity.DogProfile;
 import com.dangdangsalon.domain.dogprofile.entity.DogProfileFeature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface DogProfileFeatureRepository extends JpaRepository<DogProfileFeature, Long> {
-    Optional<List<DogProfileFeature>> findByDogProfile(DogProfile dogProfile);
+
+    @Query("SELECT dpf FROM DogProfileFeature dpf " +
+            "JOIN FETCH dpf.feature " +
+            "WHERE dpf.dogProfile = :dogProfile")
+    Optional<List<DogProfileFeature>> findByDogProfile(@Param("dogProfile") DogProfile dogProfile);
 }

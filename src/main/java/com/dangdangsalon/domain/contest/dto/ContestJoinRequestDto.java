1d40cc26a36091d161d67956a3cd71ed43384998
이도현 <lucky77795@naver.com>
2024-11-21 17:21:33 +0900

    Chore: RequestBody와의 명확한 매핑을 위해 변경

--- src/main/java/com/dangdangsalon/domain/contest/dto/ContestJoinRequestDto.java
+++ src/main/java/com/dangdangsalon/domain/contest/dto/ContestJoinRequestDto.java
@@ -1,12 +1,16 @@
 package com.dangdangsalon.domain.contest.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class ContestJoinRequestDto {
+
+    @JsonProperty("groomer_profile_id")
     private Long groomerProfileId;
+
     private String dogName;
     private String imageUrl;
     private String description;

fc3316ff3919d13476516d7d4d909d33d758f0b0
tkamo2006 <tkamo2006@naver.com>
2024-11-21 11:17:19 +0900

    Feat: 소셜 로그인 유저에서 userId 꺼내오기

--- src/main/java/com/dangdangsalon/domain/groomerprofile/review/controller/ReviewController.java
+++ src/main/java/com/dangdangsalon/domain/groomerprofile/review/controller/ReviewController.java
@@ -1,11 +1,13 @@
 package com.dangdangsalon.domain.groomerprofile.review.controller;
 
+import com.dangdangsalon.domain.auth.dto.CustomOAuth2User;
 import com.dangdangsalon.domain.groomerprofile.review.dto.ReviewInsertRequestDto;
 import com.dangdangsalon.domain.groomerprofile.review.dto.ReviewUpdateRequestDto;
 import com.dangdangsalon.domain.groomerprofile.review.service.ReviewService;
 import com.dangdangsalon.util.ApiUtil;
 import com.dangdangsalon.util.ApiUtil.ApiSuccess;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,13 +16,12 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewController {
     private final ReviewService reviewService;
 
-
     /*
     내가 쓴 리뷰 조회
-    토큰으로 유저 ID 뽑아올 예정
      */
     @GetMapping("")
-    public ApiSuccess<?> getUserReview(@RequestParam Long userId) {
+    public ApiSuccess<?> getUserReview(@AuthenticationPrincipal CustomOAuth2User user) {
+        Long userId = user.getUserId();
         return ApiUtil.success(reviewService.getUserReviews(userId));
     }
 
@@ -34,32 +35,35 @@ public class ReviewController {
 
     /*
     리뷰 등록
-    토큰으로 유저 ID 뽑아올 예정
      */
     @PostMapping("/{profileId}")
-    public ApiSuccess<?> insertReview(@RequestParam Long userId, @PathVariable Long profileId,
+    public ApiSuccess<?> insertReview(@AuthenticationPrincipal CustomOAuth2User user, @PathVariable Long profileId,
                                               @RequestBody ReviewInsertRequestDto reviewInsertRequestDto) {
+        Long userId = user.getUserId();
+
         reviewService.insertReview(userId, profileId, reviewInsertRequestDto);
         return ApiUtil.success("리뷰 등록이 완료되었습니다.");
     }
 
     /*
     리뷰 수정
-    토큰으로 유저 ID 뽑아올 예정
      */
     @PutMapping("/{reviewId}")
-    public ApiSuccess<?> updateReview(@RequestParam Long userId, @PathVariable Long reviewId,
+    public ApiSuccess<?> updateReview(@AuthenticationPrincipal CustomOAuth2User user, @PathVariable Long reviewId,
                                       @RequestBody ReviewUpdateRequestDto reviewUpdateRequestDto) {
+        Long userId = user.getUserId();
+
         reviewService.updateReview(userId, reviewId, reviewUpdateRequestDto);
         return ApiUtil.success("리뷰 수정이 완료되었습니다.");
     }
 
     /*
     리뷰 삭제
-    토큰으로 유저 ID 뽑아올 예정
      */
     @DeleteMapping("/{reviewId}")
-    public ApiSuccess<?> deleteReview(@RequestParam Long userId, @PathVariable Long reviewId) {
+    public ApiSuccess<?> deleteReview(@AuthenticationPrincipal CustomOAuth2User user, @PathVariable Long reviewId) {
+        Long userId = user.getUserId();
+
         reviewService.deleteReview(userId, reviewId);
         return ApiUtil.success("리뷰 삭제가 완료되었습니다.");
     }

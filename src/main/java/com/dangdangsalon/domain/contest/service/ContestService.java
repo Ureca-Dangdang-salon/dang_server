e9c94c2a9f163a13041e08a6e0c2a4bb718daadf
이도현 <lucky77795@naver.com>
2024-11-22 11:35:20 +0900

    Feat: 기타 랭킹 조회 캐시 추가

--- src/main/java/com/dangdangsalon/domain/contest/service/ContestService.java
+++ src/main/java/com/dangdangsalon/domain/contest/service/ContestService.java
@@ -14,6 +14,7 @@ import com.dangdangsalon.domain.contest.repository.ContestRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -64,6 +65,7 @@ public class ContestService {
     }
 
     @Transactional(readOnly = true)
+    @Cacheable(value = "winnerRank", key = "'getWinnerAndRankPost'")
     public WinnerRankDto getWinnerAndRankPost() {
         Contest previousContest = contestRepository.findPreviousContest()
                 .orElseThrow(() -> new IllegalArgumentException("지난 콘테스트가 없습니다."));

3c237e858f6a80ba8752b4651daa955c0b6aa280
99MinSu <dlalstn1023@naver.com>
2024-11-26 19:52:38 +0900

    Refactor: DogAge 컬럼명 변경으로 인한 V6 수정

--- src/main/resources/db/migration/V6__ADD_orders_column.sql
+++ src/main/resources/db/migration/V6__ADD_orders_column.sql
@@ -1,5 +1,11 @@
-ALTER TABLE orders ADD COLUMN `toss_order_id` varchar(255) ;
+ALTER TABLE orders ADD COLUMN `toss_order_id` VARCHAR(255);
 
 ALTER TABLE orders DROP COLUMN `status`;
 
-ALTER TABLE orders ADD COLUMN `status` enum('PENDING','ACCEPTED','REJECTED') DEFAULT NULL;
+ALTER TABLE orders ADD COLUMN `status` ENUM('PENDING', 'ACCEPTED', 'REJECTED') DEFAULT NULL;
+
+ALTER TABLE dog_profile DROP COLUMN `year`;
+ALTER TABLE dog_profile DROP COLUMN `month`;
+
+ALTER TABLE dog_profile ADD COLUMN `dog_year` INT;
+ALTER TABLE dog_profile ADD COLUMN `dog_month` INT;

41aea34138b20fde581eba73bf6f2dfd459bcd7b
이도현 <lucky77795@naver.com>
2024-11-27 11:55:21 +0900

    Fix: 직렬화/역직렬화 문제
    생성자 문제

--- src/main/java/com/dangdangsalon/domain/chat/dto/ChatMessageDto.java
+++ src/main/java/com/dangdangsalon/domain/chat/dto/ChatMessageDto.java
@@ -1,11 +1,15 @@
 package com.dangdangsalon.domain.chat.dto;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessageDto {
 
     private Long roomId;

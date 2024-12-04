28c3e96f8822a532b68514230ec3aabbabbf7b6f
이도현 <lucky77795@naver.com>
2024-12-04 22:25:35 +0900

    Test: Chat Service 테스트 추가 구현

--- src/test/java/com/dangdangsalon/domain/chat/service/ChatMessageServiceTest.java
+++ src/test/java/com/dangdangsalon/domain/chat/service/ChatMessageServiceTest.java
@@ -122,4 +122,95 @@ class ChatMessageServiceTest {
         then(chatRedisUtil).should(times(1)).deleteRoomData(1L);
         then(chatMessageMongoService).should(times(1)).deleteChatMessages(1L);
     }
+
+    @Test
+    @DisplayName("이전 메시지 조회 테스트 - Redis에 데이터 존재")
+    void testGetPreviousMessages_WithRedisData() {
+        ChatMessageDto message1 = ChatMessageDto.builder()
+                .sequence(1L)
+                .messageText("Redis 메시지 1")
+                .build();
+
+        given(chatRedisUtil.getFirstLoadedSequence(1L, 1L)).willReturn(5L);
+        given(chatRedisUtil.getMessagesBySequence(1L, 0L, 4L))
+                .willReturn(List.of(message1));
+        given(objectMapper.convertValue(message1, ChatMessageDto.class)).willReturn(message1);
+
+        List<ChatMessageDto> messages = chatMessageService.getPreviousMessages(1L, 1L);
+
+        assertThat(messages).hasSize(1);
+        assertThat(messages.get(0).getMessageText()).isEqualTo("Redis 메시지 1");
+        then(chatRedisUtil).should(times(1)).updateFirstLoadedSequence(1L, 1L, 1L);
+    }
+
+    @Test
+    @DisplayName("이전 메시지 조회 테스트 - MongoDB에서 데이터 조회")
+    void testGetPreviousMessages_WithMongoData() {
+        ChatMessageDto message = ChatMessageDto.builder()
+                .sequence(1L)
+                .messageText("Mongo 메시지")
+                .build();
+
+        given(chatRedisUtil.getFirstLoadedSequence(1L, 1L)).willReturn(5L);
+        given(chatRedisUtil.getMessagesBySequence(1L, 0L, 4L))
+                .willReturn(List.of());
+        given(chatMessageMongoService.getMessagesBeforeSequence(1L, 5L, ChatConst.MESSAGE_GET_LIMIT.getCount()))
+                .willReturn(List.of(message));
+
+        List<ChatMessageDto> messages = chatMessageService.getPreviousMessages(1L, 1L);
+
+        assertThat(messages).hasSize(1);
+        assertThat(messages.get(0).getMessageText()).isEqualTo("Mongo 메시지");
+        then(chatRedisUtil).should(times(1)).updateFirstLoadedSequence(1L, 1L, 1L);
+    }
+
+    @Test
+    @DisplayName("MongoDB에 모든 메시지 저장 테스트")
+    void testSaveAllMessagesMongo() {
+        ChatMessageDto redisMessage = ChatMessageDto.builder()
+                .sequence(1L)
+                .messageText("Redis 메시지")
+                .build();
+        List<Object> redisMessages = List.of(redisMessage);
+
+        given(chatRedisUtil.getAllRoomKeys()).willReturn(List.of("room:1"));
+        given(chatRedisUtil.extractRoomIdFromKey("room:1")).willReturn(1L);
+        given(chatRedisUtil.getAllMessagesFromRoom("room:1")).willReturn(redisMessages);
+        given(objectMapper.convertValue(redisMessage, ChatMessageDto.class)).willReturn(redisMessage);
+
+        chatMessageService.saveAllMessagesMongo();
+
+        then(chatMessageMongoService).should(times(1)).saveChatMessage(redisMessage);
+        then(chatRedisUtil).should(times(1)).deleteRoomData(1L);
+    }
+
+    @Test
+    @DisplayName("마지막 메시지 조회 테스트 - Redis에서 데이터 존재")
+    void testGetLastMessage_WithRedisData() {
+        ChatMessageDto redisMessage = ChatMessageDto.builder()
+                .messageText("Redis 메시지")
+                .build();
+
+        given(chatRedisUtil.getLastMessage(1L)).willReturn(redisMessage);
+
+        String lastMessage = chatMessageService.getLastMessage(1L);
+
+        assertThat(lastMessage).isEqualTo("Redis 메시지");
+    }
+
+    @Test
+    @DisplayName("마지막 메시지 조회 테스트 - MongoDB에서 데이터 조회")
+    void testGetLastMessage_WithMongoData() {
+        ChatMessageDto mongoMessage = ChatMessageDto.builder()
+                .messageText("Mongo 메시지")
+                .build();
+
+        given(chatRedisUtil.getLastMessage(1L)).willReturn(null);
+        given(chatMessageMongoService.getChatMessagesInMongo(1L, 0, 1))
+                .willReturn(List.of(mongoMessage));
+
+        String lastMessage = chatMessageService.getLastMessage(1L);
+
+        assertThat(lastMessage).isEqualTo("Mongo 메시지");
+    }
 }

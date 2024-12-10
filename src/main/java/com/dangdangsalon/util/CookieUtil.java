1dd03809beae26758d1ca054dbe6fe4b4460d557
이도현 <lucky77795@naver.com>
2024-12-10 11:30:22 +0900

    Refactor: Domain 추가

--- src/main/java/com/dangdangsalon/util/CookieUtil.java
+++ src/main/java/com/dangdangsalon/util/CookieUtil.java
@@ -18,6 +18,7 @@ public class CookieUtil {
 
         return ResponseCookie.from(key, value)
                 .path("/")
+                .domain(".dangdang-salon.com")
                 .maxAge(60 * 60 * 24 * 30)
                 .httpOnly(true)
                 .secure(true)

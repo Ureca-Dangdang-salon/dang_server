e5ce65562b95560b90d29851b1b1b5cb3e63897d
이도현 <lucky77795@naver.com>
2024-12-17 03:43:13 +0900

    Fix: ImageKey가 문제인지 테스트

--- src/main/java/com/dangdangsalon/domain/mypage/dto/req/CommonProfileRequestDto.java
+++ src/main/java/com/dangdangsalon/domain/mypage/dto/req/CommonProfileRequestDto.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class CommonProfileRequestDto {
-    private String imageKey;
+    private String profileImage;
     private String email;
     private Long districtId;
 }
